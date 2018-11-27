90b1dd10b3bc914904585e258d92c2f5fe44216d
ThammanoonAMON <42894739+ThammanoonAMON@users.noreply.github.com>
2018-11-27 23:00:43 +0700

    file model

---
+++ models/profile.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  }
+});
+module.exports = profile = mongoose.model("profile", ProfileSchema);
